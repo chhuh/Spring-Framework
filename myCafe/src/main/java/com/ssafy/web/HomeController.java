d46e9df68dd3cd7c49051eaf5d3592a65053d303
chhuh <huhc1125@naver.com>
2022-04-17 16:13:05 +0900

    new

---
+++ myCafe/src/main/java/com/ssafy/web/HomeController.java
@@ -0,0 +1,12 @@
+package com.ssafy.web;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+
+	// 아무 메소드 없어도 됨
+}
